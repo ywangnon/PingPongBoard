e9c7c035ecb30dc3d45b16ab955663e0f6794bc9
ywangnon <ywangnon123@gmail.com>
2020-05-04 22:35:25 +0900

    add manual

---
+++ PingPongScoreBoard/ManualViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ManualViewController.swift
+//  PingPongScoreBoard
+//
+//  Created by Hansub Yoo on 2020/05/04.
+//  Copyright © 2020 Hansub Yoo. All rights reserved.
+//
+
+import UIKit
+
+class ManualViewController: UIViewController {
+    var manualView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
