f8fae5698bc2763c15a47e34967cee01c429a88b
ywangnon <ywangnon123@gmail.com>
2020-06-21 22:47:52 +0900

    xib 연결

---
+++ PingPongScoreBoard/ManualBoard/FunctionDescriptionView.swift
@@ -0,0 +1,24 @@
+//
+//  FunctionDescriptionView.swift
+//  PingPongScoreBoard
+//
+//  Created by Hansub Yoo on 2020/06/21.
+//  Copyright © 2020 Hansub Yoo. All rights reserved.
+//
+
+import UIKit
+
+class FunctionDescriptionView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func initFromXIB() {
+        
+    }
+}
